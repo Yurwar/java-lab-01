e4902d50651165591ef25631d8752a62c0fffdd7
Yurii Matora <yura.matora11@gmail.com>
2021-04-20 01:07:53 +0300

    [TESTDATA] Create simple test lab with all tasks

---
+++ src/main/java/com/simplab/testdata/CommonTasks.java
@@ -0,0 +1,19 @@
+package com.simplab.testdata;
+
+public class CommonTasks {
+    public int sum(int a, int b) {
+        return 0;
+    }
+
+    public int product(int a, int b) {
+        return 0;
+    }
+
+    public int division(int a, int b) {
+        return 0;
+    }
+
+    public int pow(int a, int b) {
+        return 0;
+    }
+}
