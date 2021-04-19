0c4016d8207745db4f9130f2451a75131c5710d4
Yurii Matora <yura.matora11@gmail.com>
2021-04-20 01:09:34 +0300

    [TESTDATA] Leave last two tasks for second test variant

--- src/main/java/com/simplab/testdata/CommonTasks.java
+++ src/main/java/com/simplab/testdata/CommonTasks.java
@@ -1,14 +1,6 @@
 package com.simplab.testdata;
 
 public class CommonTasks {
-    public int sum(int a, int b) {
-        return 0;
-    }
-
-    public int product(int a, int b) {
-        return 0;
-    }
-
     public int division(int a, int b) {
         return 0;
     }
